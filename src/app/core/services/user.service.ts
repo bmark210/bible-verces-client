826883dd253caf0f604e66746d2383dc5049958e
Mark <markbaltenko@gmail.com>
2024-11-04 15:09:46 +0200

    fix base url value

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const baseUrl = process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:3000';
+const baseUrl = 
+  typeof window !== 'undefined' && process.env.NODE_ENV === 'production' 
+    ? window.location.origin 
+    : process.env.NODE_ENV === 'production'
+    ? process.env.PRODUCTION_API_URL 
+    : 'http://localhost:3000';
 
 export async function getUsers() {
   const response = await axios.get(`${baseUrl}/api/users`);
--- src/app/core/services/verses.service.ts
+++ src/app/core/services/verses.service.ts
@@ -2,8 +2,12 @@ import axios from "axios";
 import { VerseBody } from "../interfaces";
 import { Verse } from "@prisma/client";
 
-const baseUrl = process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:3000';
-
+const baseUrl = 
+  typeof window !== 'undefined' && process.env.NODE_ENV === 'production' 
+    ? window.location.origin 
+    : process.env.NODE_ENV === 'production'
+    ? process.env.PRODUCTION_API_URL 
+    : 'http://localhost:3000';
 
 export async function getVerses(): Promise<Verse[]> {
   const response = await axios.get(`${baseUrl}/api/verses`);
@@ -13,14 +17,3 @@ export async function getVerses(): Promise<Verse[]> {
 export async function createVerse(body: VerseBody) {
   return await axios.post(`${baseUrl}/api/verses`, body);
 }
-
-// import { db } from "@/api/db";
-// import { VerseBody } from "../interfaces";
-
-// export function getVerses() {
-//   return db.selectFrom("verses").selectAll().execute();
-// }
-
-// export function createNewVerse(verse: VerseBody) {
-//   return db.insertInto("verses").values(verse).returningAll().execute();
-// }
