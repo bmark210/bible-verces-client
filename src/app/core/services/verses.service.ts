6071063dc46512cb07654ab2513357030abb3431
Mark <markbaltenko@gmail.com>
2024-11-04 15:49:57 +0200

    fix base url value

--- src/app/core/services/verses.service.ts
+++ src/app/core/services/verses.service.ts
@@ -1,15 +1,17 @@
-// import axios from "axios";
-// import { VerseBody } from "../interfaces";
-// import { Verse } from "@prisma/client";
+import axios from "axios";
+import { VerseBody } from "../interfaces";
+import { Verse } from "@prisma/client";
 
-// const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.PRODUCTION_API_URL
+    : "http://localhost:3000";
 
+export async function getVerses(): Promise<Verse[]> {
+  const response = await axios.get(`${baseUrl}/api/verses`);
+  return response.data;
+}
 
-// export async function getVerses(): Promise<Verse[]> {
-//   const response = await axios.get(`${baseUrl}/api/verses`);
-//   return response.data;
-// }
-
-// export async function createVerse(body: VerseBody) {
-//   return await axios.post(`${baseUrl}/api/verses`, body);
-// }
+export async function createVerse(body: VerseBody) {
+  return await axios.post(`${baseUrl}/api/verses`, body);
+}
